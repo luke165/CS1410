da379be62c6da85a7c84349f9feb19f8542cf18f
luke165 <104848037+luke165@users.noreply.github.com>
2025-08-26 18:05:02 +0000

    Finished example0101

---
+++ src/example0101.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "enter x:";
+    cin >> x;
+
+    cout << "enter y: ";
+    cin >> y;
+    
+    cout << "x + y = " << (x + y) << endl;
+    cout << "x - y = " << (x - y) << endl;
+    cout << "x * y = " << (x * y) << endl;
+    cout << "x / y = " << (x / y) << endl;
+
+    return 0;
+
+}
