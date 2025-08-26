609ab6903064000a7674da4d3aaf71fc308f2439
luke165 <104848037+luke165@users.noreply.github.com>
2025-08-26 18:12:26 +0000

    celcius to fahrenheit

---
+++ src/ex0102.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double c, f;
+    cout << "temputure in Celcius: " << endl;
+    cin >> c;
+
+    f = 9.0 * c / 5.0 + 32;
+    cout << "tempature in Fahrenheit: " << f << endl;
+
+    return 0;
+}
