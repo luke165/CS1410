9ce0af2382b1b8622ca798cf074fa30cf186f203
luke165 <104848037+luke165@users.noreply.github.com>
2025-09-17 02:44:48 +0000

    finished example 0301

--- src/ex0301.cpp
+++ src/ex0301.cpp
@@ -3,10 +3,28 @@
 
 using namespace std;
 string repChar(char , int); //prototype
-long hms_2_secs(int, int, int);
+long hms_2_secs(int h, int m, int s);
+void swapV(double& x, double& y);
+
+void swapV(double& x, double& y){
+    double t = x;
+    x = y;
+    y = t;
+}
+
+long hms_2_secs(int h, int m, int s){
+    long secs = h * 60 * 60 + m * 60 +s;
+    return secs;
+}
 
 int main(){
     cout <<repChar('X', 8);
+    cout << hms_2_secs(4, 56, 17) << "seconds" << endl;
+    
+    double x = 10.0, y = 30.0;
+    swapV(x, y);
+    cout << x << "," << y << endl;
+    
     return 0;
 }
 
