1607dba39019cce0adfeb4edb98ee95bde4ce5df
luke165 <104848037+luke165@users.noreply.github.com>
2025-11-04 04:13:14 +0000

    done with week 7 plus some extra stuff I worked on

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(){
-    // TODO
-    std::cout << "Hello World!" << std::endl;
-    
+// Example function: f(x) = 5x^2 + x^5 + 3x^4
+double f(double x) {
+    return 5*pow(x, 2) + pow(x, 5) + 3*pow(x, 4);
+}
+
+// Difference quotient: (f(x + h) - f(x)) / h
+double differenceQuotient(double x, double h) {
+    return (f(x + h) - f(x)) / h;
+}
+
+int main() {
+    double x = 795.2321;   // point where we want the slope
+    double h = 1e-5;  // small step size
+
+    double dq = differenceQuotient(x, h);
+
+    cout << "The difference quotient at x = " << x
+         << " is approximately " << dq << endl;
+
     return 0;
 }
